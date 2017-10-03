eccba27f323570038f2ad5a31299e0d33dcc2227
Olegka95 <Oleg95ul@mail.ru>
2017-10-03 23:56:44 +0400

    Add geters and seters to Book's fields, release createFictionalData() method.

--- app/src/main/java/com/simbirsoft_mobile_revolution2017_v_team/mobilelibrary/Book.java
+++ app/src/main/java/com/simbirsoft_mobile_revolution2017_v_team/mobilelibrary/Book.java
@@ -8,6 +8,7 @@ import java.util.Date;
 
 public class Book {
 
+    private int id;
     private String name;
     private String author;
     private Date year;
@@ -28,5 +29,65 @@ public class Book {
         this.ISBN = ISBN;
         this.numberOfPages = numberOfPages;
         this.isAvailable = isAvailable;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getYear() {
+        return year;
+    }
+
+    public void setYear(Date year) {
+        this.year = year;
+    }
+
+    public String getPublishingHouse() {
+        return publishingHouse;
+    }
+
+    public void setPublishingHouse(String publishingHouse) {
+        this.publishingHouse = publishingHouse;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
     }
 }
--- app/src/main/java/com/simbirsoft_mobile_revolution2017_v_team/mobilelibrary/Library.java
+++ app/src/main/java/com/simbirsoft_mobile_revolution2017_v_team/mobilelibrary/Library.java
@@ -1,7 +1,12 @@
 package com.simbirsoft_mobile_revolution2017_v_team.mobilelibrary;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by Denis on 03.10.2017.
@@ -9,14 +14,14 @@ import java.util.List;
 
 public class Library {
 
-    private List<Book> books  = new ArrayList<>();
+    private List<Book> books = new ArrayList<>();
 
     public void loadLibrary(){
         createFictionData();
     }
 
     public Book getBookWithIndex(int index) {
-        if(books == null){
+        if(books.size() == 0){
             return null;
         }
         if((index < 0) || (index >= books.size())){
@@ -26,6 +31,31 @@ public class Library {
     }
 
     private void createFictionData(){
-        //fiction books
+        books.add(new Book("Снежная Королева", "Ханс Андерсен", createDateFromString("2017"), "Верже ИД", "9785906928030", 72, true));
+        books.add(new Book("Незнакомка", "Анна Рэй", createDateFromString("2017"), "Альфа-книга", "978-5-9922-2492-4", 314, true));
+        books.add(new Book("Портрет с пулей в челюсти", "Ханна Кралль", createDateFromString("2017"), "Corpus", "978-5-17-098823-5", 384, true));
+        books.add(new Book("Старик и шляпа", "Игорь Шевчук", createDateFromString("2017"), "Мещерякова ИД", "978-5-00108-134-0", 32, true));
+        books.add(new Book("Как я была маленькой", "Вера Желиховская", createDateFromString("2017"), "Речь", "978-5-9268-2583-8", 208, true));
+        books.add(new Book("Лесная газета. Осень", "Виталий Бианки", createDateFromString("2017"), "Мещерякова ИД", "978-5-00108-128-9", 96, true));
+        books.add(new Book("Исчезнувший мир", "Игорь Акимушкин", createDateFromString("2017"), "Мещерякова ИД", "978-5-00108-095-4", 192, true));
+        books.add(new Book("Цена вопроса", "Александра Маринина", createDateFromString("2017"), "Эксмо", "978-5-04-004675-1", 352, true));
+        books.add(new Book("В тихом омуте", "Пола Хокинс", createDateFromString("2017"), "АСТ", "978-5-17-104797-9", 384, true));
+        books.add(new Book("Шепот в темноте", "Елена Звездная", createDateFromString("2017"), "Эксмо", "978-5-699-96975-3", 352, true));
+        books.add(new Book("Июнь", "Дмитрий Быков", createDateFromString("2017"), "Редакция Елены Шубиной", "978-5-17-092368-7", 512, true));
+        books.add(new Book("Остров Сокровищ", "Роберт Стивенсон", createDateFromString("2017"), "Лабиринт", "978-5-9287-2526-6", 188, true));
+    }
+
+    private Date createDateFromString(String year) {
+        DateFormat dateformat = new SimpleDateFormat("yyyy");
+        try {
+            return dateformat.parse(year);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<Book> getBooks() {
+        return books;
     }
 }
